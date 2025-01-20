3296ae88a73fb179c17df4ceb87e1f8c20415921
Lucas DEL CERRO <lucas.del.cerro@etu.univ-st-etienne.fr>
2025-01-20 03:58:23 +0100

    plus que le port

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -23,8 +23,7 @@ class BlogController extends AbstractController
                     'Cloud computing'
                 ],
                 'social' => [
-                    'github' => 'https://github.com/lucasdelcerro',
-                    'linkedin' => 'https://linkedin.com/in/lucas-delcerro'
+                    'github' => 'https://github.com/LB69200',
                 ]
             ]
         ]);
@@ -33,7 +32,55 @@ class BlogController extends AbstractController
     #[Route('/about', name: 'app_about')]
     public function about(): Response
     {
-        return $this->render('blog/about.html.twig');
+        return $this->render('blog/about.html.twig', [
+            'interests' => [
+                [
+                    'category' => 'Technical',
+                    'items' => [
+                        [
+                            'name' => 'Développement Python',
+                            'icon' => 'fab fa-python',
+                            'description' => 'Passion pour le développement Python, particulièrement dans l\'automatisation et les scripts réseau.'
+                        ],
+                        [
+                            'name' => 'Sécurité Réseau',
+                            'icon' => 'fas fa-shield-alt',
+                            'description' => 'Intérêt marqué pour la cybersécurité et la protection des infrastructures réseau.'
+                        ],
+                        [
+                            'name' => 'Administration Système',
+                            'icon' => 'fas fa-server',
+                            'description' => 'Configuration et maintenance des systèmes Linux et Windows Server.'
+                        ],
+                        [
+                            'name' => 'Cloud Computing',
+                            'icon' => 'fas fa-cloud',
+                            'description' => 'Exploration des technologies cloud et des services AWS.'
+                        ]
+                    ]
+                ],
+                [
+                    'category' => 'Personal',
+                    'items' => [
+                        [
+                            'name' => 'Apprentissage Continu',
+                            'icon' => 'fas fa-book',
+                            'description' => 'Toujours en quête de nouvelles connaissances et de perfectionnement technique.'
+                        ],
+                        [
+                            'name' => 'Projets Open Source',
+                            'icon' => 'fas fa-code-branch',
+                            'description' => 'Contribution et participation active à la communauté open source.'
+                        ],
+                        [
+                            'name' => 'Résolution de Problèmes',
+                            'icon' => 'fas fa-puzzle-piece',
+                            'description' => 'Passion pour la résolution de défis techniques complexes.'
+                        ]
+                    ]
+                ]
+            ]
+        ]);
     }
 
     #[Route('/cv', name: 'app_cv')]
@@ -45,7 +92,17 @@ class BlogController extends AbstractController
     #[Route('/portfolio', name: 'app_portfolio')]
     public function portfolio(): Response
     {
-        return $this->render('blog/portfolio.html.twig');
+        return $this->render('blog/portfolio.html.twig', [
+            'portfolio' => [
+                'iframes' => [
+                    'https://docs.google.com/document/d/XXXXX1/preview', // Infrastructure Réseau
+                    'https://docs.google.com/document/d/XXXXX2/preview', // Sécurité Réseau
+                    'https://docs.google.com/document/d/XXXXX3/preview', // Développement Python
+                    'https://docs.google.com/document/d/XXXXX4/preview', // Cloud Computing
+                    'https://docs.google.com/document/d/XXXXX5/preview'  // Virtualisation
+                ]
+            ]
+        ]);
     }
 
     #[Route('/cv/download', name: 'app_cv_download')]
