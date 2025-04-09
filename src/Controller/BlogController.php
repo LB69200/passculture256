9c66ce2ff74be45e35ceb5132aa0122d310287c0
LB Labourgade <lucas.del.cerro@etu.univ-st-etienne.fr>
2025-04-09 07:45:06 +0200

    final

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -89,11 +89,79 @@ class BlogController extends AbstractController
         ]);
     }
 
+    #[Route('/poursuite', name: 'app_poursuite')]
+    public function poursuite(): Response
+    {
+        return $this->render('blog/poursuite.html.twig', [
+            'cybersecurity' => [
+                'title' => 'Spécialisation en Cybersécurité',
+                'description' => 'Mon projet de poursuite d\'études dans le domaine de la cybersécurité',
+                'motivations' => [
+                    [
+                        'title' => 'Passion pour la sécurité informatique',
+                        'icon' => 'fas fa-shield-alt',
+                        'description' => 'Depuis mes débuts en réseaux et télécommunications, la sécurité des systèmes d\'information est devenue une véritable passion.'
+                    ],
+                    [
+                        'title' => 'Un secteur en pleine expansion',
+                        'icon' => 'fas fa-chart-line',
+                        'description' => 'Face à l\'évolution constante des menaces, les experts en cybersécurité sont de plus en plus recherchés sur le marché du travail.'
+                    ],
+                    [
+                        'title' => 'Compétences techniques adaptées',
+                        'icon' => 'fas fa-laptop-code',
+                        'description' => 'Mon parcours en R&T m\'a permis d\'acquérir des bases solides en réseaux, systèmes et programmation, essentielles pour me spécialiser en sécurité.'
+                    ]
+                ],
+                'skills' => [
+                    [
+                        'name' => 'Analyse de vulnérabilités',
+                        'percentage' => 75
+                    ],
+                    [
+                        'name' => 'Pentesting',
+                        'percentage' => 65
+                    ],
+                    [
+                        'name' => 'Sécurité réseau',
+                        'percentage' => 80
+                    ],
+                    [
+                        'name' => 'Cryptographie',
+                        'percentage' => 60
+                    ],
+                    [
+                        'name' => 'Sécurité Cloud',
+                        'percentage' => 70
+                    ]
+                ],
+                'projects' => [
+                    [
+                        'title' => 'Projet CTF universitaire',
+                        'description' => 'Participation à des compétitions Capture The Flag, renforçant mes compétences en identification de vulnérabilités.'
+                    ],
+                    [
+                        'title' => 'Laboratoire de sécurité virtuel',
+                        'description' => 'Mise en place d\'un environnement virtualisé pour pratiquer des techniques de détection et de prévention d\'intrusion.'
+                    ],
+                    [
+                        'title' => 'Script d\'analyse de logs',
+                        'description' => 'Développement d\'un outil Python pour l\'analyse automatisée de journaux de sécurité et la détection d\'anomalies.'
+                    ]
+                ],
+                'formation' => [
+                    'title' => 'Master Cybersécurité',
+                    'description' => 'Mon objectif est d\'intégrer un Master spécialisé en Cybersécurité pour approfondir mes connaissances et me préparer aux défis de demain dans ce domaine en constante évolution.'
+                ]
+            ]
+        ]);
+    }
+
     #[Route('/cv/download', name: 'app_cv_download')]
     public function downloadCV(): Response
     {
         $cvPath = $this->getParameter('kernel.project_dir') . '/public/cv/mon-cv.pdf';
-        
+
         return $this->file($cvPath);
     }
 }
